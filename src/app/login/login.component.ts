e5e9dded0b7f7d19cd62acf76a70226abafb4b43
AndresOtero14 <andresoterolobo@gmail.com>
2023-01-31 23:01:04 -0500

    actualizacion 1

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+
+
+  constructor(private readonly fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.initFrom();
+  }
+
+
+  onSubmit(): void { };
+
+  initFrom(): FormGroup {
+   return this.fb.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required, Validators.minLength(8)]
+    })
+  }
+
+}
